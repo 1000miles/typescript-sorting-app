22f2d7e73caf361049df5b4b1e88e2eb3cc7dba5
Thuy Le <gitnap@mailbox.org>
2020-02-10 00:46:18 +0100

    S10/L89 - Just one more fix & exclude some dirs to compile

--- build/index.js
+++ build/index.js
@@ -1,13 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
-var CharactersCollection_1 = require("./CharactersCollection");
+var LinkedList_1 = require("./LinkedList");
 // const numbersCollection = new NumbersCollection([200, 3, -5, 0]);
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log(numbersCollection.data);
-var charactersCollection = new CharactersCollection_1.CharactersColllection('Xaayb');
-// Pass in charactersCollection with Sortable interface
-var sorter = new Sorter_1.Sorter(charactersCollection);
+// const charactersCollection = new CharactersColllection('Xaayb');
+// // Pass in charactersCollection with Sortable interface
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
+var linkedList = new LinkedList_1.LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+var sorter = new Sorter_1.Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
+linkedList.print();
+// [start:run] -10
+// [start:run] -3
+// [start:run] 4
+// [start:run] 500
--- build/src/index.js
+++ build/src/index.js
@@ -1,30 +1,21 @@
-import { Sorter } from './Sorter';
-import { NumbersCollection } from './NumbersCollection';
-import { CharactersColllection } from './CharactersCollection';
-import { LinkedList } from './LinkedList';
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Sorter_1 = require("./Sorter");
+var LinkedList_1 = require("./LinkedList");
 // const numbersCollection = new NumbersCollection([200, 3, -5, 0]);
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log(numbersCollection.data);
-
 // const charactersCollection = new CharactersColllection('Xaayb');
 // // Pass in charactersCollection with Sortable interface
 // const sorter = new Sorter(charactersCollection);
 // sorter.sort();
 // console.log(charactersCollection.data);
-
-const linkedList = new LinkedList();
+var linkedList = new LinkedList_1.LinkedList();
 linkedList.add(500);
 linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
-
-const sorter = new Sorter(linkedList);
+var sorter = new Sorter_1.Sorter(linkedList);
 sorter.sort();
 linkedList.print();
-
-// [start:run] -10
-// [start:run] -3
-// [start:run] 4
-// [start:run] 500
