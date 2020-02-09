4a9087e6fbec866f732d733942ade0c35d33b709
Thuy Le <gitnap@mailbox.org>
2020-02-09 20:59:15 +0100

    S10/L77 - Sorting Implementation

--- build/index.js
+++ build/index.js
@@ -9,7 +9,28 @@ var Sorter = /** @class */ (function () {
     function Sorter(collection) {
         this.collection = collection;
     }
+    // We don't return anything here
     Sorter.prototype.sort = function () {
+        var length = this.collection.length; // aquivalent to: const length = this.collection.length;
+        /**
+         * BUBBLE SORT: Compare a pair of elements (number) per each iteration
+         * in ascending order (default)
+         */
+        // First loop
+        for (var i = 0; i < length; i++) {
+            // Second loop: right most element will be in the sorted position after first iteration is done which is why "i -1" is substracted here from length
+            for (var j = 0; j < length - i - 1; j++) {
+                // If left hand-side element (j) is greater than right-hand side element (j + 1)
+                if (this.collection[j] > this.collection[j + 1]) {
+                    // Swap elements: Assign left-hand side element (j) to a new variable (leftHand)
+                    var leftHand = this.collection[j];
+                    // Switch right-hand side element to left-hand side: j = j + 1
+                    this.collection[j] = this.collection[j + 1];
+                    // Swap old left-hand side and move it to last right-hand side element
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
     };
     return Sorter;
 }());
--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,29 @@ class Sorter {
 
   }
 
+  // We don't return anything here
   sort(): void {
+    const { length } = this.collection; // aquivalent to: const length = this.collection.length;
 
+    /**
+     * BUBBLE SORT: Compare a pair of elements (number) per each iteration
+     * in ascending order (default)
+     */
+    // First loop
+    for (let i = 0; i < length; i++) {
+      // Second loop: right most element will be in the sorted position after first iteration is done which is why "i -1" is substracted here from length
+      for (let j = 0; j < length - i - 1; j++) {
+        // If left hand-side element (j) is greater than right-hand side element (j + 1)
+        if (this.collection[j] > this.collection[j + 1]) {
+          // Swap elements: Assign left-hand side element (j) to a new variable (leftHand)
+          const leftHand = this.collection[j];
+          // Switch right-hand side element to left-hand side: j = j + 1
+          this.collection[j] = this.collection[j + 1];
+          // Swap old left-hand side and move it to last right-hand side element
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
   }
 };
 
