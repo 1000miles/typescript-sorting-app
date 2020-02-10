1908e2c6e75a683bc997c1b0832721b83642bc93
Thuy Le <gitnap@mailbox.org>
2020-02-10 01:23:18 +0100

    Uncomment numbersCollection, charactersCollection & linkedList

--- build/index.js
+++ build/index.js
@@ -1,24 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+//import { Sorter } from './Sorter';
+var NumbersCollection_1 = require("./NumbersCollection");
+var CharactersCollection_1 = require("./CharactersCollection");
 var LinkedList_1 = require("./LinkedList");
-// const numbersCollection = new NumbersCollection([200, 3, -5, 0]);
-// numbersCollection.sort(); // Fix: Can not ceate instance on abstract class
-// // const sorter = new Sorter(numbersCollection);
-// // sorter.sort();
-// console.log(numbersCollection.data);
-// const charactersCollection = new CharactersColllection('Xaayb');
-// charactersCollection.sort(); // Fix: Can not ceate instance on abstract class
-// // Pass in charactersCollection with Sortable interface
-// // const sorter = new Sorter(charactersCollection);
-// // sorter.sort();
-// console.log(charactersCollection.data);
+var numbersCollection = new NumbersCollection_1.NumbersCollection([200, 3, -5, 0]);
+numbersCollection.sort(); // Fix: Can not ceate instance on abstract class
+console.log(numbersCollection.data);
+var charactersCollection = new CharactersCollection_1.CharactersColllection('Xaayb');
+charactersCollection.sort(); // Fix: Can not ceate instance on abstract class
+console.log(charactersCollection.data);
 var linkedList = new LinkedList_1.LinkedList();
 linkedList.add(500);
 linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
-// const sorter = new Sorter(linkedList);
-// sorter.sort();
 linkedList.sort(); // Fix: Can not ceate instance on abstract class
 linkedList.print();
 // [start:run] -10
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,21 @@
-import { Sorter } from './Sorter';
+//import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersColllection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
-// const numbersCollection = new NumbersCollection([200, 3, -5, 0]);
-// numbersCollection.sort(); // Fix: Can not ceate instance on abstract class
-// // const sorter = new Sorter(numbersCollection);
-// // sorter.sort();
-// console.log(numbersCollection.data);
+const numbersCollection = new NumbersCollection([200, 3, -5, 0]);
+numbersCollection.sort(); // Fix: Can not ceate instance on abstract class
+console.log(numbersCollection.data);
 
-// const charactersCollection = new CharactersColllection('Xaayb');
-// charactersCollection.sort(); // Fix: Can not ceate instance on abstract class
-// // Pass in charactersCollection with Sortable interface
-// // const sorter = new Sorter(charactersCollection);
-// // sorter.sort();
-// console.log(charactersCollection.data);
+const charactersCollection = new CharactersColllection('Xaayb');
+charactersCollection.sort(); // Fix: Can not ceate instance on abstract class
+console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(500);
 linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
-
-// const sorter = new Sorter(linkedList);
-// sorter.sort();
 
 linkedList.sort(); // Fix: Can not ceate instance on abstract class
 linkedList.print();
